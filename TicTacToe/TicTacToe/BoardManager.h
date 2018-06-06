684e950aa4a1922b81b148d449f2b0f3db88b9ec
Jonathan Cook <cookj12@coventry.ac.uk>
2018-06-06 13:53:29 +0100

    added manager classes for network, board and game

---
+++ TicTacToe/TicTacToe/BoardManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class BoardManager
+{
+public:
+	BoardManager();
+	~BoardManager();
+};
+
---
+++ TicTacToe/TicTacToe/NetworkManager.cpp
@@ -0,0 +1,12 @@
+#include "NetworkManager.h"
+
+
+
+NetworkManager::NetworkManager()
+{
+}
+
+
+NetworkManager::~NetworkManager()
+{
+}
