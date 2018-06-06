76d6d567c3b9610e369133b32fda8cf3d47d4b0b
Jonathan Cook <cookj12@coventry.ac.uk>
2018-06-06 14:59:23 +0100

    added placeholder sprites, drawn to screen and moving, added basic chat groundwork

--- TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,24 +1,64 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+bool chatInput = false;
 
 int main(int argc, char** argv)
 {
-	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!", sf::Style::Default);//setting style gives us options of what the window has at the top
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
+	sf::Texture crossTx;
+	if (!crossTx.loadFromFile("../sprites/cross.png"))
+	{
+		// error...
+	}
+
+	sf::Sprite crossSp;
+	crossSp.setTexture(crossTx);
+	sf::Vector2f crossVeloss(1, 0);//naming is why
+
 	while (window.isOpen())
 	{
+		crossSp.move(crossVeloss);
+		if (crossSp.getPosition().x > 100)
+			crossVeloss.x = -1;
+		else if (crossSp.getPosition().x < 0)
+			crossVeloss.x = 1;
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (event.type)
+			{
+				// window closed
+			case sf::Event::Closed:
 				window.close();
+				break;
+
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Enter && chatInput == true)
+				{
+					std::cout << "enter" << std::endl;
+					chatInput = false;
+				}
+				else if (event.key.code == sf::Keyboard::T && chatInput == false)
+				{
+					std::cout << "We Chat Bois" << std::endl;
+					chatInput = true;
+				}
+
+			case sf::Event::TextEntered:
+				if (event.text.unicode < 128 && chatInput == true)
+					std::cout << "ASCII character typed: " << static_cast<char>(event.text.unicode) << std::endl;
+
+			default:
+				break;
+			}
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(crossSp);
 		window.display();
 	}
 }
